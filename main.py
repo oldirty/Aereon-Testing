50e89b6b0bd1ed48cda56b6f6aa8d8bbaf993aec
Zach Morgan <zmorgan@mdot-ps.co>
2021-08-28 15:14:39 -0400

    auto linting with `black`

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-#module Imports
+# module Imports
 import discord
 import os, requests, json
 import math, time, datetime
@@ -11,23 +11,26 @@ intents = discord.Intents.default()
 intents.members = True
 client = commands.Bot(command_prefix="!", intents=intents)
 
+
 @client.event
 async def on_ready():
     print("Aeron Flight Bot v1.0")
 
-if __name__ == '__main__':
-  client.add_cog(Altf4(client))
 
-  @client.event
-  async def on_member_join(member):
-    channel = client.get_channel(865731272919744535)
-    await channel.send(
-      "Welcome to AltF4 Airlines discord! When ready head down to ACAP for a interview! Good luck!")
+if __name__ == "__main__":
+    client.add_cog(Altf4(client))
 
-  @client.event
-  async def on_member_remove(member):
-    channel = client.get_channel(865731272919744535)
-    await channel.send("Hah, later nerd!")
+    @client.event
+    async def on_member_join(member):
+        channel = client.get_channel(865731272919744535)
+        await channel.send(
+            "Welcome to AltF4 Airlines discord! When ready head down to ACAP for a interview! Good luck!"
+        )
 
-  keep_alive()
-  client.run(os.environ['envtoken'])
+    @client.event
+    async def on_member_remove(member):
+        channel = client.get_channel(865731272919744535)
+        await channel.send("Hah, later nerd!")
+
+    keep_alive()
+    client.run(os.environ["envtoken"])
